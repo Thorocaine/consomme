e6815655dc95b057331a751c1e8cf1d2f47823f1
Jonathan Peel <me@jonathanpeel.co.za>
2019-01-27 10:39:23 +0200

    pushed to npm

--- src/lib/Div.test.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import Div from './Div';
-
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Div />, div);
-  ReactDOM.unmountComponentAtNode(div);
-});
--- src/lib/Div.tsx
+++
@@ -1,7 +0,0 @@
-import React, { HTMLProps, HTMLAttributes } from "react";
-
-const Div = () => (
-  <div>MY DIV</div>
-);
-
-export default Div;
